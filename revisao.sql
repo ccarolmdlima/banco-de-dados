c383ee2a7029830bdbe7febf211a58d6d77b3844
Carolina <126472626+ccarolmdlima@users.noreply.github.com>
2024-06-20 00:24:19 -0300

    Update revisao.sql

--- revisao.sql
+++ revisao.sql
@@ -35,10 +35,33 @@ Ordenação: Crescente pelo nome do usuário.
 MER FÍSICO DO BANCO DE DADOS
 
 USUARIO                                            EMPRESTIMO
-CODIGO  NUMBER    <pk>  <-----FK_EMPR_USUARIO----  CODIGO          NUMBER      <pk>  not null
-NOME    VARCHAR2(80)                               DT_EMPRESTIMO   DATE              not null
-                                                   DT_DEVOLUCAO    DATE              null
-BIBLIOTECARIO                                      OBESVACOES      VARCHAR2(200)     null
-CODIGO  NUMBER    <pk> <--FK_EMPR_BIBLIOTECARIO--  
-NOME    VARCHAR2(80)
+CODIGO  NUMBER    <pk>        FK_EMPR_USUARIO      CODIGO          NUMBER      <pk>   not null
+NOME    VARCHAR2(80)                               DT_EMPRESTIMO   DATE               not null
+                                                   DT_DEVOLUCAO    DATE               null
+BIBLIOTECARIO                                      OBESVACOES      VARCHAR2(200)      null
+CODIGO  NUMBER    <pk>    FK_EMPR_BIBLIOTECARIO    CD_USR          NUMBER      <fk2>  not null
+NOME    VARCHAR2(80)                               CD_BIB          NUMBER      <fk3>  not null
+                                                   CD_LIV          NUMBER      <fk1>  not null
+                                                   
+                                                   FK_EMPR_LIVRO
+                                                   
+AUTOR                                              LIVRO
+CODIGO  NUMBER  <pk>  not null                     CODIGO       NUMBER         <pk>
+NOME    VARCHAR2(80)  not null                     VOLUME       VARCHAR2(3)
+                                                   PAGINAS      NUMBER(3)
+                                                   SITUACAO     VARCHAR2(500)
+                                                   OBSERVACOES  VARCHAR2(500)
+                                                   CD_PUB       NUMBER          <fk>
+                                                   
+FK_PUB_AUTOR                                       FK_LIVRO_PUBLICACAO
+
+                                                   PUBLICACAO
+                                                   CODIGO          NUMBER          <pk>   not null                      TITULO
+                                                   ANO             VARCHAR2(4)            not null     FK_PUB_TITULO    CODIGO      NUMBER          <pk>
+                                                   EDICAO          NUMBER(2)              not null                      DESCRICAO   VARCHAR2(100)
+                                                   AREA            VARCHAR2(50)           null
+                                                   RESUMO          VARCHAR2(2000)         null                          CIDADE
+                                                   CD_TIT          NUMBER          <fk2>  not null     FK_PUB_CIDADE    CODIGO  NUMBER        <pk>
+                                                   CD_CID          NUMBER          <fk1>  not null                      NOME    VARCHAR2(60)
+                                                   CD_AUT          NUMBER          <fk3>  not null                      UF      VARCHAR2(2)
 */
